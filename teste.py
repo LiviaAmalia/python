35bd573c1105326fb30115622a81da9f065c8578
AmaliaLivia <amalialivia058@gmail.com>
2024-06-12 14:32:08 -0300

    first commit

--- teste.py
+++ teste.py
@@ -1,6 +1,27 @@
-nome = "lívia"
-idade= 16
-altura=1.59
-menor_de_idade= idade>=16
-print("Nome:", nome, "Idade", idade, "Altura", altura )
-print("É menor de idade ?", menor_de_idade)
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 3 # float
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 3
+print('Divisão inteira', divisao_inteira)
+
+exponciacao = 2 ** 10
+print('Exponciação', exponciacao)
+
+modulo = 55 % 2 # resto da divisão
+print('Módulo', modulo)
+
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
+print(16 % 2 == 0)
+
